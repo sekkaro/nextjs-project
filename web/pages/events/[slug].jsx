6cde6dd6eb440852844f9aa8610a15e6cb2f12e9
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2021-08-16 12:16:32 +0900

    layout, header & footer

--- web/pages/events/[slug].jsx
+++ web/pages/events/[slug].jsx
@@ -1,5 +1,11 @@
+import Layout from "../../components/Layout";
+
 const EventPage = () => {
-  return <div>My Event</div>;
+  return (
+    <Layout>
+      <h1>My Event</h1>
+    </Layout>
+  );
 };
 
 export default EventPage;
--- web/pages/events/add.jsx
+++ web/pages/events/add.jsx
@@ -1,5 +1,11 @@
+import Layout from "../../components/Layout";
+
 const AddEventPage = () => {
-  return <div>Add Event</div>;
+  return (
+    <Layout title="Add New Event">
+      <h1>Add Event</h1>
+    </Layout>
+  );
 };
 
 export default AddEventPage;
