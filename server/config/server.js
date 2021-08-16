4c53b15b1592bfe122b289be3fd6c34663a88e8e
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2021-08-16 20:30:16 +0900

    strapi cms setup

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0d193ca18286d65b13df95059e713e22'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c59752a1-1e80-4401-8a85-a70d2ea7e0a8'
+};
